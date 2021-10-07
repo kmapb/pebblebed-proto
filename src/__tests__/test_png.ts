431ca925a4885737bf192e4e9d289f41b46f9f59
Keith Adams <kmadams@gmail.com>
2021-10-07 09:45:09 -0700

    More progress serialization.

--- src/__tests__/test_png.ts
+++ src/__tests__/test_png.ts
@@ -2,11 +2,17 @@ import { layer } from '../Png';
 import { PNG } from 'pngjs';
 const fs = require('fs');
 
-test('We can smooosh up heartz.', () => {
-   var heart = fs.createReadStream('../../assets/tests/heart.png'); 
-   heart.on('parsed', (hrt) => {
-       console.log("fun times!");
+test('We can smooosh up heartz.', done => {
+   return fs.createReadStream(__dirname + '/../../assets/test/heart.png')
+   .pipe(
+       new PNG({
+           filterType: 4,
+       })
+   ).on('parsed', hrt => {
+       console.log("parsed!");
        console.log(hrt.height);
        console.log(hrt.width);
+       hrt.pack().pipe(fs.createWriteStream('/tmp/out.png'));
+       done();
    });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import serve from './GraphQL';
+const http = require('http');
 
 function App() {
     console.log('fun times!');
+    const hostname = '127.0.0.1';
+    const port = 3000;
+
+    const server = http.createServer((req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Hello World');
+      });      
+
     serve();
 }
 
